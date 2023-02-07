dc32536db6c2af286ca22efb5208f3abe6c5c833
Tomas23052 <aluno23052@ipt.pt>
2023-02-07 21:33:26 +0000

    password change teste#2

--- app/routes/auth.routes.js
+++ app/routes/auth.routes.js
@@ -21,5 +21,5 @@ module.exports = function(app) {
 
   app.post("/api/auth/signin", controller.signin);
 
-  app.patch("/api/auth/:id");
+  app.patch("/api/auth/:id", controller.passwordChange);
 };
